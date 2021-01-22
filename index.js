8807a7a32d7dfb230458a0d0059f881409d3f860
Abd <abdelbasset>
2021-01-22 11:15:11 +0200

    11

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
+  console.log(req.files);
+  console.log(req);
   if (req.files) {
     console.log(req.files);
     var file = req.files.file;
     var filename = file.name;
     console.log(filename);
-    //hey
+
+    //console.log("type", file.type);
+    //console.log("size", file.size);
 
     file.mv("./uploads/videos/" + filename, function (err) {
       writeToDB(filename);
@@ -58,3 +62,32 @@ function readFromDB() {
     });
   });
 }
+
+// const buildHairstyleCard = (name, favourites) => {
+//   // Create elements needed to build a card
+//   const div = document.createElement("div");
+//   const h4 = document.createElement("h4");
+//   const a = document.createElement("a");
+//   const img = document.createElement("img");
+//   // Append newly created elements into the DOM
+//   const body = document.querySelector("body");
+//   body.append(div);
+//   h4.append(a);
+//   div.append(h4);
+//   div.append(img);
+//   // Set content and attributes
+//   a.innerHTML = name;
+//   img.setAttribute("src", "/uploads/thumbnails/" + name + ".png");
+//   div.setAttribute("class", "card");
+// };
+
+// try {
+//   db.serialize(function () {
+//     db.each("SELECT * FROM videos", function (err, row) {
+//       console.log("Name: " + row.name + "\nFavourites: " + row.favourites);
+//       data.forEach((hairstyle) => buildHairstyleCard(row.name, row.favourites));
+//     });
+//   });
+// } catch (error) {
+//   console.error(error);
+// }
